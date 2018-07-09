3809d5001391474bd40a464f65ff755cb2c86aa4
Keebler17 <realawesomeperson493@gmail.com>
2018-07-09 10:48:53 -0400

    commited all files lol

---
+++ src/io/github/Keebler17/chars/Chars.java
@@ -0,0 +1,38 @@
+package io.github.Keebler17.chars;
+
+import java.util.Random;
+
+public class Chars {
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			System.out.println("Generates random characters.\nUsage: chars [int]");
+			System.exit(0);
+		} else {
+			try {
+				Random r = new Random();
+				int length = Integer.valueOf(args[0]);
+				char chars[] = new char[length];
+				
+				int random = 0;
+
+				for(int i = length-1; i >= 0; i--) {
+					// Generate random number in range of ASCII numbers
+					while(random < 33 && random != 127) {
+						random = r.nextInt(127);
+					}
+					
+					chars[i] = (char)random;
+					random = 0;
+				}
+				System.out.println(chars);
+				System.exit(0);
+				
+				System.exit(0);
+			} catch(Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+}
+;
